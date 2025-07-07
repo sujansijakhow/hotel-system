c3909fc2fb9bfe5f584245b91725d613a6ac2352
sujansijakhow <sujansijakhow@gmail.com>
2025-07-07 15:57:10 +0545

    tailwind configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // this line includes .js, .jsx, .ts, .tsx inside /src and its subfolders
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#4A6FA5',  // primary
+          light: '#3B82F6',    // lighter shade
+          dark: '#1E40AF',     // darker shade
+        },
+        secondary:{
+          DEFAULT: '#545158',
+        },
+        fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      },
+    },
   },
   plugins: [],
 }
