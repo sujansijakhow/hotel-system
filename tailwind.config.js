52fba8b2072b6f64d95e64fe42863338529d4451
sujansijakhow <sujansijakhow@gmail.com>
2025-07-07 15:10:03 +0545

    configuration updated

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // this line includes .js, .jsx, .ts, .tsx inside /src and its subfolders
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
