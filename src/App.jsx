7aeeda1bfd4b26709dc3f8f6f20e5e46aba776ec
sujansijakhow <sujansijakhow@gmail.com>
2025-07-09 13:45:01 +0545

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
 
   return (
-    <>
+   <div className="flex gap-2">
       <Sidebar />
-    </>
+      <Navbar />
+   </div>
   )
 }
 
