e101005f520ea246468f176e1fa98c82dce819d2
sujansijakhow <sujansijakhow@gmail.com>
2025-07-11 14:57:07 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Home from "./routes/Home";
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    }
+  ])
+
   return (
-   <div className="flex gap-">
-      <Sidebar />
-      <Navbar />
-   </div>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
