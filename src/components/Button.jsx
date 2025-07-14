70cd7c75abf7aeb165c0979fb37702fa339834ad
sujansijakhow <sujansijakhow@gmail.com>
2025-07-14 16:15:34 +0545

    Updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Button = ({
   name,
   height = 40,
   width = 120,
+  to
 }) => {
   return (
-    <button
-      style={{ height: `${height}px`, width: `${width}px` }}
-      className="bg-primary rounded-md font-medium text-white text-[14px] border p-1"
-    >
-      {name}
-    </button>
+    <Link to={to}>
+      <button
+        style={{ height: `${height}px`, width: `${width}px` }}
+        className="bg-primary rounded-md font-medium text-white text-[14px] border p-1"
+      >
+        {name}
+      </button>
+    </Link>
   );
 };
 
