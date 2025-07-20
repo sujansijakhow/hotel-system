44d1a9cb4a197069f26b336be7a6682d3d4a1a43
sujansijakhow <sujansijakhow@gmail.com>
2025-07-20 14:14:24 +0545

    Updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const Sidebar = () => {
+    const location = useLocation();
+    const path = location.pathname;
+
+    const isMyBooking = path === '/mybooking';
+
     return (
         <div className='w-[270px] font-inter bg-white border rounded-tr-md rounded-br-md h-[590px] drop-shadow-xl'>
             <div className='flex flex-col gap-6'>
@@ -9,29 +15,43 @@ const Sidebar = () => {
                     <h1 className='text-primary font-bold text-[24px]'>Golden Haven Hotel</h1>
                     <div className='border-b-2 border-[#F5F5F5] w-[250px]'></div>
                 </div>
-                <div className='flex items-center gap-3 ml-6'>
-                    <p>🔍</p>
-                    <span className='text-primary'>Search Hotels</span>
-                </div>
-                <div className='flex items-center gap-3 ml-6'>
-                    <p>🔖</p>
-                    <span className='text-secondary'>My Bookings</span>
-                </div>
+
+                {/* Search Hotels */}
+                <Link to={"/"}>
+                    <div className='flex items-center gap-3 ml-6'>
+                        <p>🔍</p>
+                        <span className={isMyBooking ? 'text-secondary' : 'text-primary'}>
+                            Search Hotels
+                        </span>
+                    </div>
+                </Link>
+
+                {/* My Bookings */}
+                <Link to={"/mybooking"}>
+                    <div className='flex items-center gap-3 ml-6'>
+                        <p>🔖</p>
+                        <span className={isMyBooking ? 'text-primary' : 'text-secondary'}>
+                            My Bookings
+                        </span>
+                    </div>
+                </Link>
+
+                {/* Hotel Lists */}
                 <div className='flex items-center gap-3 ml-6'>
                     <p>📃</p>
                     <span className='text-secondary'>Hotel lists</span>
                 </div>
             </div>
+
             <div className='flex flex-col gap-6 mt-72'>
                 <div className='ml-2 border-b-2 border-[#F5F5F5] w-[250px]'></div>
                 <div className='flex items-center gap-3 ml-6'>
                     <p>⏫</p>
                     <span className='text-secondary'>Logout</span>
                 </div>
-
             </div>
         </div>
     )
 }
 
-export default Sidebar
+export default Sidebar;
